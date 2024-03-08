53f024fdb8b906f7ade0b46a61813c050a7c6911
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-03-09 00:30:24 +0300

    Error throwing fix.

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,42 +1,45 @@
 /* eslint-disable no-underscore-dangle */
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
-  }
+    this._name = '';
+    this._length = 0;
+    this._students = [];
 
-  set name(value) {
-    if (typeof value === 'string') {
-      this._name = value;
-    } else {
-      console.error('TypeError: Name must be a string');
-    }
-  }
-
-  set length(value) {
-    if (typeof value === 'number') {
-      this._length = value;
-    } else {
-      console.error('TypeError: Length must be a number');
-    }
-  }
-
-  set students(value) {
-    if (Array.isArray(value)) {
-      this._students = value;
-    }
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  get lengthHBC() {
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  get length() {
     return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
   }
 
   get students() {
     return this._students;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value) || !value.every((item) => typeof item === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
   }
 }
