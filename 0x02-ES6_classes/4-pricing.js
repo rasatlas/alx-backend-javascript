4583740c75d457b72d455adf40a1f8383e9d355f
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-03-09 01:27:41 +0300

    Addint static method to a class.

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-underscore-dangle */
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+}
