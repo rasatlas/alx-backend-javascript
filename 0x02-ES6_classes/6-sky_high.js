49d4f217b18a595d6cfc03cfe46690072a3aaacf
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-03-09 22:58:15 +0300

    Extending and implementing a class and a method.

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,30 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors
+  }
+
+  //getter and setter
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  //method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
