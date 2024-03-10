abd5cc5cb0279714a71d7096c1c396aa10599c14
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-03-10 21:32:40 +0300

    using Symbols.

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-underscore-dangle */
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
+
+export default Car;
