e5a07db4a575914f67f34567ee66cd3e08984479
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-03-12 01:21:17 +0300

    Driver for 3-get_ids_sum.js

---
+++ 0x03-ES6_data_manipulation/3-main.js
@@ -0,0 +1,8 @@
+/* eslint-disable jest/require-hook */
+import getListStudents from './0-get_list_students';
+import getStudentIdsSum from './3-get_ids_sum';
+
+const students = getListStudents();
+const value = getStudentIdsSum(students);
+
+console.log(value);
