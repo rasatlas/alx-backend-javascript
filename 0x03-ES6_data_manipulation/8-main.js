8b6786a501cc3ccc66e9201f77192320c2a28ec2
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-03-12 22:38:12 +0300

    Driver for 8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable jest/require-hook */
+import cleanSet from './8-clean_set';
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
