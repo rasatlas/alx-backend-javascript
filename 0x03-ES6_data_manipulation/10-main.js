6cd6b06eaed6a324190f106ee3a64b39da257d0f
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-03-12 23:05:07 +0300

    Driver for 10-update_uniq_items.js

---
+++ 0x03-ES6_data_manipulation/10-main.js
@@ -0,0 +1,9 @@
+/* eslint-disable jest/require-hook */
+import updateUniqueItems from './10-update_uniq_items';
+import groceriesList from './9-groceries_list';
+
+const map = groceriesList();
+console.log(map);
+
+updateUniqueItems(map);
+console.log(map);
