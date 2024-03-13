09f4a59502f8cb26598650c2c63b033ec1272339
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-03-13 22:43:50 +0300

    Interface design and implementation.

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,33 @@ interface Teacher {
   location: string;
   [key: string]: any;
 }
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+interface StudentClass {
+  firstName: string;
+  lastName: string;
+  workOnHomeworkFunction: () => string;
+  displayNameFunction: () => string;
+}
+
+class StudentClass implements StudentClass {
+  constructor(public firstName: string, public lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
